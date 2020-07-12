dba40939d6a6c1b2fbe07b0c7eb807a2e7c070a3
Codeblogger <m.furkangulsen@hotmail.com>
2020-07-12 13:56:28 +0300

    yolo

--- yolo_for_image.py
+++ yolo_for_image.py
@@ -19,7 +19,7 @@ ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 
-image = cv2.imread("images/room.jpg")
+image = cv2.imread("images/football.jpg")
 (H, W) = image.shape[:2]
 
 
@@ -37,7 +37,7 @@ for output in layerOutputs:
 	for detection in output:
 		scores = detection[5:]
 		classID = np.argmax(scores)
-		confidence = scores[classID]
+		confidence = scores[classID]        
 		if confidence > 0.50:
 			box = detection[0:4] * np.array([W, H, W, H])
 			(centerX, centerY, width, height) = box.astype("int")
@@ -46,7 +46,7 @@ for output in layerOutputs:
 			boxes.append([x, y, int(width), int(height)])
 			confidences.append(float(confidence))
 			classIDs.append(classID)
-
+            
 
 idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
 if len(idxs) > 0:
@@ -54,10 +54,10 @@ if len(idxs) > 0:
 		(x, y) = (boxes[i][0], boxes[i][1])
 		(w, h) = (boxes[i][2], boxes[i][3])
 
-		cv2.rectangle(image, (x, y), (x + w, y + h), (0,255,0), 2)
+		cv2.rectangle(image, (x, y), (x + w, y + h), (0,255,255), 2)
 		text = "{}: {:.2f}".format(labels[classIDs[i]], confidences[i])
 		cv2.putText(image, text, (x, y - 7), cv2.FONT_HERSHEY_SIMPLEX, 
-              0.5, (0,255,0), 2)
+              0.5, (0,255,255), 2)
 
 cv2.imshow("Output", image)
 
